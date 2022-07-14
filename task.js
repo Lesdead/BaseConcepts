0002c8dc2d9ea210298a239d3ed830731786e22a
lesdead <lesdead@gmail.com>
2022-07-14 05:35:32 +0300

    finish? project

--- task.js
+++ task.js
@@ -1,13 +1,28 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  if (a < 0) {
+    return false;
+  }
+  let x1;
+  let x2;
+  let discriminant = (b ** 2 - 4 * a * c);
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+     x1 = arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+     return x1
+     
+  } else if (discriminant > 0) {
+    let answer = {};
+    x1 = arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    x2 = arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+    answer.firstRoot = x1;
+    answer.SecondRoot = x2;
+    return answer;
+
+  }
+
   return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
