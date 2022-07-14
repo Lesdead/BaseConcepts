0f1aab91acb375067bb919734e146ca9d0571d84
lesdead <lesdead@gmail.com>
2022-07-15 02:01:35 +0300

    project edited

--- task.js
+++ task.js
@@ -1,28 +1,17 @@
 "use strict";
+
 function solveEquation(a, b, c) {
+
   let arr = [];
-  if (a < 0) {
-    return false;
-  }
-  let x1;
-  let x2;
-  let discriminant = (b ** 2 - 4 * a * c);
+  let discriminant = Math.pow(b, 2) - 4*a*c;
 
-  if (discriminant < 0) {
-    return arr;
-  } else if (discriminant === 0) {
-     x1 = arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
-     return x1
-     
-  } else if (discriminant > 0) {
-    let answer = {};
-    x1 = arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
-    x2 = arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
-    answer.firstRoot = x1;
-    answer.SecondRoot = x2;
-    return answer;
+    if (discriminant === 0) {
+      arr.push(- b / (2 * a));
 
-  }
+    } else if (discriminant > 0) {
+      arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+      arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+      } 
 
-  return arr; // array
+  return arr; 
 }
